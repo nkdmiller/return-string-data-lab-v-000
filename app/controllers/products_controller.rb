9b5ab1943f403846333bf8d911c31f0c0e71ab7c
Nate Miller <natekmiller96@gmail.com>
2018-10-24 20:15:21 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,39 @@
 class ProductsController < ApplicationController
-  def index
-    @products = Product
-  end
-  def new
-    @product = Product.new
-  end
+    def index
+        @products = Product.all
+    end
+     def show
+        
+    end
+     def new
+        @product = Product.new
+    end
+     def create
+        @product = Product.create(product_params)
+        @product.save
+        redirect_to products_path
+     end
+     def edit
+     end
+     def update
+        @product.update(product_params)
+        redirect_to product_path(@product)
+    end
+     def inventory
+        if @product.inventory > 0
+            render plain: "true"
+        else
+            render plain: "false"
+        end
+    end
+     def description
+        render plain: @product.description
+    end
+     private
+     def set_product
+        @product = Product.find(params[:id])
+    end
+     def product_params
+        params.require(:product).permit(:name, :price, :description, :inventory)
+    end
 end
