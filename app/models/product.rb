a1f68c6d0ddb478e2e8531977f3b0bddb9320dbe
Nate Miller <natekmiller96@gmail.com>
2018-10-24 20:16:17 +0000

    automatically backed up by learn

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
-  def inventory_text
+  def inventory_check
     if self.inventory == 0
       "Sold Out"
     else
